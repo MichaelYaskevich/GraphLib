bdae6b1ae61658b923e901217a0b2e83974a8d7b
MichaelYaskevich <The_person38@mail.ru>
2022-05-17 20:29:16 +0500

    сделал набросок алгоритма форда беллмана

--- GraphLib/algorithms/pathSearch/bellman_ford.py
+++ GraphLib/algorithms/pathSearch/bellman_ford.py
@@ -1,2 +1,81 @@
-def bellman_form_shortest_path(graph, source):
-    pass
+import math
+
+from GraphLib.dataStructures.di_graph import DiGraph
+from GraphLib.dataStructures.exception import NegativeCycleError
+
+d = {}
+graph: DiGraph
+previous = {}
+
+
+def shortest_path_bellman_ford(_graph, source) -> (dict, dict):
+    '''
+    Finds shortest paths from source vertex to all other nodes.
+
+    Graph can have edges with negative weights.
+
+    :raises NegativeCycleError if there are negative cycles in graph
+
+    :param _graph: Digraph
+    :param source: first vertex in path
+    :return: shortest path tree with distances
+    '''
+
+    set_up(source)
+
+    for _ in range(1, len(graph.edges())):
+        update_distances()
+
+    find_negative_cycle()
+
+    return previous, d
+
+
+def set_up(_graph, source):
+    global graph
+    graph = _graph
+    initialise_distances(source)
+    previous[source] = None
+
+
+def initialise_distances(source):
+    for v in graph.adjacency_lists.keys():
+        d[v] = math.inf
+    d[source] = 0
+
+
+def update_distances():
+    for w, v in get_better_edges():
+        update_dist(w, v)
+
+
+def update_dist(prev, current):
+    d[current] = d[prev] + graph.edge_weight((prev, current))
+    previous[current] = prev
+
+
+def get_better_edges():
+    for w, v in graph.edges():
+        if d[w] + graph.edge_weight((w, v)) < d[v]:
+            yield w, v
+
+
+def find_negative_cycle():
+    for w, v in get_better_edges():
+        raise_error(w, v)
+
+
+def raise_error(prev, source):
+    message = '-'.join(build_cycle(prev, source))
+    raise NegativeCycleError(f"Negative cycle was found {message}")
+
+
+def build_cycle(prev, source):
+    path = [source, prev]
+    current = prev
+    while current != source:
+        current = previous[current]
+        path.append(current)
+
+    path.reverse()
+    return path
---
+++ GraphLib/dataStructures/exception.py
@@ -0,0 +1,5 @@
+class NegativeCycleError(RuntimeError):
+    '''
+    Some algorithms raise this exception when they find a negative cycle.
+    '''
+    pass

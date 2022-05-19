a7c34cb5ed84faf273bb4c008998a5aed11496bc
MichaelYaskevich <The_person38@mail.ru>
2022-05-19 20:22:15 +0500

    добавил аннотации типов в классы графов

--- GraphLib/dataStructures/di_graph.py
+++ GraphLib/dataStructures/di_graph.py
@@ -6,21 +6,21 @@ class DiGraph:
         self.adjacency_lists = {}
         self.nodes_to_edge_dict = {}
 
-    def get_nodes(self):
+    def get_nodes(self) -> list:
         return list(self.adjacency_lists.keys())
 
-    def get_edges(self):
+    def get_edges(self) -> list:
         return list(self.nodes_to_edge_dict.values())
 
-    def get_adjacent_nodes(self, node):
+    def get_adjacent_nodes(self, node) -> list:
         return self.adjacency_lists[node]
 
-    def add_node(self, node):
+    def add_node(self, node) -> None:
         if node in self.adjacency_lists:
             raise ValueError("Graph already contains this node")
         self.adjacency_lists[node] = []
 
-    def add_edge(self, edge: Edge):
+    def add_edge(self, edge: Edge) -> None:
         start, end = edge
         if start not in self.adjacency_lists or end not in self.adjacency_lists:
             raise ValueError("Start node or end node aren't present in adjacency list")
@@ -31,5 +31,5 @@ class DiGraph:
         self.adjacency_lists[start].append(end)
         self.nodes_to_edge_dict[(start, end)] = edge
 
-    def get_incident_edge(self, start, end):
+    def get_incident_edge(self, start, end) -> Edge:
         return self.nodes_to_edge_dict[(start, end)]
--- GraphLib/dataStructures/graph.py
+++ GraphLib/dataStructures/graph.py
@@ -6,21 +6,21 @@ class Graph:
         self.adjacency_lists = {}
         self.nodes_to_edge_dict = {}
 
-    def get_nodes(self):
+    def get_nodes(self) -> list:
         return list(self.adjacency_lists.keys())
 
-    def get_edges(self):
+    def get_edges(self) -> list:
         return list(self.nodes_to_edge_dict.values())
 
-    def get_adjacent_nodes(self, node):
+    def get_adjacent_nodes(self, node) -> list:
         return self.adjacency_lists[node]
 
-    def add_node(self, node):
+    def add_node(self, node) -> None:
         if node in self.adjacency_lists:
             raise ValueError("Graph already contains this node")
         self.adjacency_lists[node] = []
 
-    def add_edge(self, edge: Edge):
+    def add_edge(self, edge: Edge) -> None:
         first_node, second_node = edge
         if first_node not in self.adjacency_lists or second_node not in self.adjacency_lists:
             raise ValueError("First_node or second_node aren't present in graph")
@@ -33,5 +33,5 @@ class Graph:
         self.nodes_to_edge_dict[(first_node, second_node)] = edge
         self.nodes_to_edge_dict[(second_node, first_node)] = edge
 
-    def get_incident_edge(self, start_node, end_node):
+    def get_incident_edge(self, start_node, end_node) -> Edge:
         return self.nodes_to_edge_dict[(start_node, end_node)]

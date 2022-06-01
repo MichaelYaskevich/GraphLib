58cf3e26bca13b60a6fd0e80d11ceb38943b7095
MichaelYaskevich <The_person38@mail.ru>
2022-06-01 20:09:03 +0500

    hot_fix

--- GraphLib/interface/read_weight_matrix.py
+++ GraphLib/interface/read_weight_matrix.py
@@ -4,18 +4,18 @@ from GraphLib.dataStructures.edge import Edge
 
 def read_weight_matrix(lines):
     graph = DiGraph()
-    source = int(lines[-2]) - 1
-    destination = int(lines[-1]) - 1
+    source = lines[-2].strip()
+    destination = lines[-1].strip()
     matrix_lines = lines[1:-2]
 
     for i, line in enumerate(matrix_lines):
-        graph.add_node(i)
+        graph.add_node(str(i))
     for i, line in enumerate(matrix_lines):
         for j, value in enumerate(line.split()):
             if value == '-':
                 continue
             val = int(value)
             if val != 0:
-                graph.add_edge(Edge(i, j, val))
+                graph.add_edge(Edge(str(i), str(j), val))
 
     return graph, source, destination
